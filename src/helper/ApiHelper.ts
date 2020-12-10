403c8227657bb2f6394ca484177a9ae50b4532fa
KMS_buiphantho <buiphantho@gmail.com>
2020-12-11 00:38:46 +0700

    implement api helper to call api

---
+++ src/helper/ApiHelper.ts
@@ -0,0 +1,47 @@
+import { config } from "../config/config";
+
+export const fetchAPI = async (httpMethod: string, api: string, requestBody?: any) => {
+  let url = config.url.apiServer + api;
+
+  try {
+    if (httpMethod === "GET") {
+      if (httpMethod === "GET" && requestBody !== undefined && requestBody !== null) {
+        const encodedQueryParams = encodeURI(JSON.stringify(requestBody));
+        url += url.includes("count") ? `?where=${encodedQueryParams}` : `?filter=${encodedQueryParams}`;
+      }
+
+      const response = await fetch(url, {
+        method: httpMethod,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: ''
+        },
+      });
+
+      if (response.ok) {
+        const jsonRespon = await response.json();
+        return jsonRespon;
+      }
+    } 
+    else if (httpMethod === "POST" || httpMethod === "PATCH") {
+      const response = await fetch(url, {
+        method: httpMethod,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+          // Authorization: ''
+        },
+        body: requestBody !== undefined && requestBody !== null
+                ? JSON.stringify(requestBody)
+                : JSON.stringify({})  
+      });
+      if (response.ok) {
+        const jsonRespon = await response.json();
+        return jsonRespon;
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
