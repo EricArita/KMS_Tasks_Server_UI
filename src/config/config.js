b4bd36d814dd348b518136b5c8aaeb19d77368f9
KMS_buiphantho <buiphantho@gmail.com>
2020-12-11 00:36:03 +0700

    add configuration settings for project

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+export const config = {
+    appName: 'Kms_Tasks',
+    url: {
+      apiServer: 'https://localhost:5001/api/v1/',
+    },
+    cookies: {
+      domain: 'localhost',
+      maxAge: 60 * 60 * 1000,
+    },
+  };
+  
